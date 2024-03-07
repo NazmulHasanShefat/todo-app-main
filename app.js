86331a6819a5f1e726f76d4cc2c52316eb07404f
Nazmul <mdnazmulhasan0879@gmail.com>
2024-03-08 01:53:33 +0600

    some fix

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 let myform = document.querySelector("form");
 let fuld_text = document.getElementById("input_box");
-let todo_list = document.querySelector(".list_container");
   myform.addEventListener("submit", (e)=>{
     e.preventDefault();
     addtodu();
   });
 
   function addtodu(){
+    let todo_list = document.querySelector(".list_container");
     let toducoll = document.createElement("li");
     let todou_text = fuld_text.value;
     toducoll.innerHTML = `
-     <li class="mycheck"><div class="cercle"><img src="./images/icon-check.svg" class="check-image" alt=""></div>
-     <div class="list-content-text">${todou_text}</div>
-     <div class="crose-icon"><img src="./images/icon-cross.svg" alt=""></div></li>
+    <li class="mycheck"><div class="cercle"><img src="./images/icon-check.svg" class="check-image" alt=""></div>
+    <div class="list-content-text">list 1</div>
+    <div class="crose-icon"><img src="./images/icon-cross.svg" alt=""></div>
+    </li>
     `;
-    todo_list.appendChild();
+   todo_list.appendChild(toducoll);
   };
+  // ${todou_text}
