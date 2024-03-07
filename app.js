d9e4325cb3e20cf72891a55fbe08af2b24ddb287
Nazmul <mdnazmulhasan0879@gmail.com>
2024-03-07 17:51:58 +0600

    some modification

---
+++ app.js
@@ -0,0 +1,18 @@
+let myform = document.querySelector("form");
+let fuld_text = document.getElementById("input_box");
+let todo_list = document.querySelector(".list_container");
+  myform.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    addtodu();
+  });
+
+  function addtodu(){
+    let toducoll = document.createElement("li");
+    let todou_text = fuld_text.value;
+    toducoll.innerHTML = `
+     <li class="mycheck"><div class="cercle"><img src="./images/icon-check.svg" class="check-image" alt=""></div>
+     <div class="list-content-text">${todou_text}</div>
+     <div class="crose-icon"><img src="./images/icon-cross.svg" alt=""></div></li>
+    `;
+    todo_list.appendChild();
+  };
