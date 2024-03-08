0d875a295129a5cd329f41a1b044ed2ba7137d10
Nazmul <mdnazmulhasan0879@gmail.com>
2024-03-09 00:45:37 +0600

    some modi

--- app.js
+++ app.js
@@ -5,16 +5,30 @@ let fuld_text = document.getElementById("input_box");
     addtodu();
   });
 
-  function addtodu(){
+  let todos=JSON.parse(localStorage.getItem("todos"));
+  if(todos){
+    todos.forEach(element => {
+      addtodu(element);
+    });
+  }
+
+  function addtodu(elem){
     let todo_list = document.querySelector(".list_container");
     let toducoll = document.createElement("li");
     let todou_text = fuld_text.value;
+
+   if(elem){
+     todou_text=elem.fuld_text;
+   }
+    if(todou_text){
     toducoll.innerHTML = `
     <li class="mycheck"><div class="cercle"><img src="./images/icon-check.svg" class="check-image" alt=""></div>
-    <div class="list-content-text">${todou_text}</div> 
+    <div class="list-content-text"><p class="pTag">${todou_text}</p></div> 
     <div class="crose-icon"><img src="./images/icon-cross.svg" alt=""></div></li>
     `;
    todo_list.appendChild(toducoll);
+   updateLs();
+    }
 
    let close=toducoll.querySelector(".crose-icon");
    close.addEventListener("click", ()=>{
@@ -23,14 +37,28 @@ let fuld_text = document.getElementById("input_box");
 
    let check_todu=toducoll.querySelector(".cercle");
    let check_icon=toducoll.querySelector(".check-image");
+   let todo_text_color=toducoll.querySelector(".list-content-text");
     check_todu.addEventListener("click", ()=>{
       check_icon.classList.toggle("checked-icon");
       check_todu.classList.toggle("checked-background");
       toducoll.classList.toggle("clecked");
+      todo_text_color.classList.toggle("line-throu_text_color");
+      updateLs();
     });
   };
   
-  
+  function updateLs(){
+    let pTag =document.querySelectorAll(".pTag");
+    let arr=[];
+    pTag.forEach(element => {
+      arr.push({
+        fuld_text:element.innerText,
+        complete:element.classList.contains("checked-icon")
+
+      })
+    });
+    localStorage.setItem("todos",JSON.stringify(arr));
+  }
   
   // let check_icon = document.querySelectorAll(".check-image");
   // check_icon.forEach( function(e){
@@ -47,3 +75,10 @@ let fuld_text = document.getElementById("input_box");
   // var cros_icon = document.querySelector(".crose-icon");
 // var check_background = document.querySelector(".check-image");
 // ${todou_text}
+
+// importent
+ 
+// <li class="mycheck"><div class="cercle"><img src="./images/icon-check.svg" class="check-image" alt=""></div>
+// <div class="list-content-text"><p class="pTag ${elem && elem.complete?"complete":""}">${todou_text}</p></div> 
+// <div class="crose-icon"><img src="./images/icon-cross.svg" alt=""></div></li>
+// `;
